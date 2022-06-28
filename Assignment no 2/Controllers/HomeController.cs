065c7857ad2b1cd26efd18013759fe8c25c6f094
ut <ut@WINDOWS>
2022-06-29 03:52:16 +0500

    Add project files.

---
+++ Assignment no 2/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_no_2.Controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
